3fd397982501c3625d2234dd8a2702237ac06834
Daniel Melchior <d.melchior@me.com>
2012-11-21 19:01:05 +0000

    a bit margin for the entries in the friends list
    git-svn-id: file:///var/www/svn/ssn/branches/design2@749 94719523-d52b-487b-9aad-823e8bff2bb7

--- views/profile/friends/friends.js
+++ views/profile/friends/friends.js
@@ -3,13 +3,14 @@
 ssn.display.profile.friends = {
 	load: function (done) {
 		ssn.display.profile.user.friends(function (d) {
-			var names = $("<div>");
+			var element = $("#friendsList");
+			var names;
 			var i;
 			for (i = 0; i < d.length; i += 1) {
 				names.append($("<div>").append($("<a>").attr("href", d[i].getLink()).text(d[i].getName())));
 			}
 
-			$("#friendsList").append(names);
+			element.append(names);
 			$("body").addClass("friendsView");
 			done();
 		});
@@ -21,3 +22,11 @@ ssn.display.profile.friends = {
 		$("body").removeClass("friendsView");
 	}
 };
+
+var userElement = $("<li>").append(
+					$("<a>").attr("href", "#view=profile&userid=" + user.getUserID()).append(
+						$("<img>").addClass("friendPicture").attr("src", "img/user.png")
+					).append(
+						$("<span>").addClass("friendName").text(user.getName())
+					)
+				);
