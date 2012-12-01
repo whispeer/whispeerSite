8a7c34b9837090fcd9bd9d11c364a6f15f1bdd50
Daniel Melchior <d.melchior@me.com>
2012-12-01 11:01:39 +0000

    basic markup and style: done. structure of the js files: done. description of what has to be done in js: done.
    git-svn-id: file:///var/www/svn/ssn/branches/design2@809 94719523-d52b-487b-9aad-823e8bff2bb7

--- views/media/pictures/pictures.js
+++ views/media/pictures/pictures.js
@@ -1,9 +1,28 @@
 "use strict";
 
-ssn.display.friends.main = {
+ssn.display.media.pictures = {
+	hideAll: function () {
+		$("#mediaWrap").hide().html("");
+	},
+	
+	loadMedia: function() {
+		// TODO: load all photos of the user that is availiable!
+	},
+	
 	load: function (done) {
 		$("body").addClass("mediaView");
+		ssn.display.media.pictures.doLoad();
 		done();
+	},
+	
+	doLoad: function () {
+		try {
+			ssn.display.media.pictures.hideAll();
+			ssn.display.media.pictures.loadMedia();
+		} catch (e) {
+			ssn.logger.log(e);
+			throw e;
+		}
 	},
 
 	hashChange: function (done) {
@@ -15,4 +34,10 @@ ssn.display.friends.main = {
 	}
 };
 
-// TODO: give .droplet the css rule top with the value of the clicked .image element + 20 pixels.
+/*
+TODO: Append pictues and videos in rows and if a element is clicked switch them to active:
+how the pictures / videos have to be appended: divide the width of #mediaWrap by 355.
+The outcome will be the number of pictures that will fit in one row! It may NOT be a number like 2,3 it HAS to be a number like 2 or 3.
+Each row has an id like 1,2,3 and so on. When a image or a video of the row is clicked, the row and the element go into the active state, and the droplet will be apended under the row (so it's the next element in the code, after the div of the row is closed).
+In the droplet itself the following info has to be loaded: People on the picture eg: "Daniel Melchior (user who postet the image / video) with Igor(Link to profile) and Olaf(Link to profile)", how many awesomes? eg: "50 People think this is awesome!", how many comments? eg: "300 comments". Also the option of giving an awesome or a whatever is given.
+*/
