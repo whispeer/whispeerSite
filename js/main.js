067147cb8669a4624f856543fdbf92288065a52d
Nils Kenneweg <nk@ovt.me>
2012-12-05 11:15:14 +0000

    more going forward with modularization. added a state object, which saves current state data.
    git-svn-id: file:///var/www/svn/ssn/branches/requirejs@814 94719523-d52b-487b-9aad-823e8bff2bb7

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
-define(['jquery', 'helper/logger', 'libs/step'], function ($, logger) {
+define(['jquery', 'helper/logger', 'helper/helper', 'libs/step', 'model/state'], function ($, logger, step, state) {
 	require(['libs/sjcl'], function (sjcl) {
 		sjcl.random.startCollectors();
 	});
 
-	logger.log("starting up");
+	/**
+	*	Load function is called on document load.
+	*	Main program entry point.
+	*/
+	var load = function () {
+		logger.log("Starting up");
+		display.load();
+
+		//Local storage available?
+		if (session.storageAvailable()) {
+			// We already got a session?
+			if (session.logedin()) {
+				session.loadOldSession();
+				return;
+			}
+		} else {
+			//Display warning message.
+			display.showWarning(i18n.getValue("noSessionSaving"));
+			state.loaded = true;
+			$(window).trigger('hashchange');
+		}
+
+		display.loadView("register");
+	};
+
+	step(function startUp() {
+		$(document).ready(this);
+	}, h.sF(function domReady() {
+		load();
+	}));
+
 });
---
+++ js/model/state.js
@@ -0,0 +1,3 @@
+define({
+	loaded: false
+});
