619c4f009aa695226ddec16924881666f0356b44
Nils Kenneweg <software@ovt.me>
2013-03-26 22:18:50 +0100

    keystore rewritten once again. use strict.

--- js/asset/socket.js
+++ js/asset/socket.js
@@ -1,4 +1,5 @@
 define([], function () {
+	"use strict";
 	var connected = false;
 
 	var requestListener = [];
--- js/crypto/keyStore.js
+++ js/crypto/keyStore.js
@@ -1,26 +1,100 @@
-define([], function () {
-	var keys = {};
+/** our keystore.
+	handles all the keys, passwords, etc.
+	keys are a little difficult because some keys look different but are the same because they are encrypted differently
+	also keys always have a decryptor because the are never distributed alone.
+**/
+define(["libs/step", "asset/helper"], function (step, h) {
+	"use strict";
+	var symKeys = {};
+	var asymKeys = {};
 	var passwords = [];
 
-	var keyStore = {
-		encrypt: function (text, keyID) {
-		
-		},
-		
-		decrypt: function (ctext, keyID) {
-		
-		},
-		
-		reset: function () {
-			keys = {};
-			passwords = [];
-		},
-		
-		generateKey: function (parentKeyID) {
-		
+	var symKey = {
+		fetch: function (keyid, callback) {
+
 		}
 	};
+
+
+	function asymKeyFetch(keyid, callback) {
+
+	}
 	
+	function asymKeyGenerate() {
 	
+	}
+	
+	var asymKey = function (keydata) {
+		function decryptF() {
+		
+		}
+
+		this.decrypt = decryptF;
+	};
+
+	asymKey.fetch = asymKeyFetch;
+	asymKey.generate = asymKeyGenerate;
+
+	var keyStore = {
+		reset: function reset() {
+			asymKeys = {};
+			symKeys = {};
+			passwords = [];
+		},
+
+		sym: {
+			generateKey: function (parentKeyID, callback) {
+				var cryptKey;
+
+				step(function () {
+					symKey.generateKey(this);
+				}, callback);
+			},
+			symEncryptKey: function (realKeyID, parentKeyIDs) {
+				
+			},
+			asymEncryptKey: function (realKeyID, parentKeyIDs) {
+
+			},
+			generatePWEncryptedKey: function (password) {
+
+			},
+			encrypt: function (text, realKeyID) {
+
+			},
+			decrypt: function (ctext, realKeyID) {
+
+			}
+		},
+
+		asym: {
+			generateKey: function () {
+
+			},
+			symEncryptKey: function (realKeyID, parentKeyIDs) {
+
+			},
+			asymEncryptKey: function (realKeyID, parentKeyIDs) {
+
+			},
+			generatePWEncryptedKey: function (password) {
+
+			},
+			encrypt: function (text, keyID) {
+
+			},
+			decrypt: function (ctext, keyID) {
+
+			}
+		},
+		
+		sign: {
+			generateKey: function () {
+			
+			}
+		}
+	};
+
+
 	return keyStore;
 });
