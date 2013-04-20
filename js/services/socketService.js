f675532baa96fc81847ccd4775bd5d44850177f4
Nils Kenneweg <nk@ovt.me>
2013-04-20 11:53:05 +0200

    socketService started. node: socket.io dep added.

---
+++ js/services/socketService.js
@@ -0,0 +1,21 @@
+/**
+* LoginService
+**/
+define(['angular'], function (angular) {
+	"use strict";
+
+	var service = function () {
+		return {
+			listen: function (channel) {
+			
+			},
+			send: function (channel, data) {
+			
+			}
+		}
+	};
+
+	service.$inject = [];
+
+	return service;
+});
