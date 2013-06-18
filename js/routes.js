9f4eb3d7e3129e20767f203601878e9414cab7e5
Nils Kenneweg <nk@ovt.me>
2013-06-18 16:12:55 +0200

    new routes.

--- js/routes.js
+++ js/routes.js
@@ -2,9 +2,25 @@ define(['angular', 'app'], function(angular, app) {
 	'use strict';
 
 	return app.config(['$routeProvider', function($routeProvider) {
+		/** login and register */
 		$routeProvider.when('/login', {
-			templateUrl: 'views/login/main/login.html',
+			templateUrl: 'views/login/login.html',
 			controller: 'ssn.loginController'
+		});
+
+		/** after login */
+		$routeProvider.when('/main', {
+			templateUrl: 'views/main/main.html',
+			controller: 'ssn.mainController'		
+		});
+		$routeProvider.when('/user/:userid', {
+			templateUrl: 'views/user/user.html',
+			controller: 'ssn.userController'		
+		});
+		$routeProvider.when('/:identifier', {
+			redirectTo: function (params) {
+				return '/user/' + params["identifier"]	;
+			}
 		});
 		$routeProvider.otherwise({redirectTo: '/login'});
 	}]);
