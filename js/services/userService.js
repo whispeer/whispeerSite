492b756672ca0cec192af976d59f5794bdbf3c40
Nils Kenneweg <nk@ovt.me>
2013-08-03 23:56:33 +0200

    userservice started.

--- js/services/userService.js
+++ js/services/userService.js
@@ -4,13 +4,26 @@
 /**
 * SessionService
 **/
-define([], function () {
+define(["step"], function (step) {
 	"use strict";
 
-	var service = function () {
+	var service = function (socketService) {
+		var User = function (identifier) {
+			function loadData(cb) {
+				step(function () {
+					socketService.emit("user.get", {identifier: identifier}, this);
+				}, cb);
+			}
+
+			this.getNickname = function (cb) {
+				loadData(cb);
+			};
+		};
+
+		return User;
 	};
 
-	service.$inject = [];
+	service.$inject = ["ssn.socketService"];
 
 	return service;
 });
