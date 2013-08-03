b019587684eb9ad91ddaf60aea381c9fa8913979
Nils Kenneweg <software@ovt.me>
2013-08-04 01:33:58 +0200

    improvement.

--- js/services/userService.js
+++ js/services/userService.js
@@ -9,10 +9,19 @@ define(["step"], function (step) {
 
 	var service = function (socketService) {
 		var User = function (identifier) {
+			var data;
 			function loadData(cb) {
 				step(function () {
-					socketService.emit("user.get", {identifier: identifier}, this);
-				}, cb);
+					if (data) {
+						this.last.ne(data);
+					} else {
+						socketService.emit("user.get", {identifier: identifier}, this);
+					}
+				}, h.sF(function (userData) {
+					data = userData;
+
+					this.last.ne(data);
+				}), cb);
 			}
 
 			this.getNickname = function (cb) {
