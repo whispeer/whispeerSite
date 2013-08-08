a355c39ba95de366f48a8edf5660776eb5ec6997
Michelle Thenhausen <michelle.thenhausen@teleos-web.de>
2013-08-08 19:02:21 +0200

    Translations for the MagicBar actions

--- js/i18n/l_de.js
+++ js/i18n/l_de.js
@@ -24,7 +24,7 @@
 	},
 	"wall": {
 		"postit": {
-			"receiver": "Wer soll dienen Beitrag lesen können?",
+			"receiver": "Wer soll deinen Beitrag lesen können?",
 			"text": "Was möchtest du den Leuten mitteilen?",
 			"send": "Senden"
 		},
@@ -117,8 +117,153 @@
 							"awesomed":	"{user} hat ein Foto von {owner} kommentiert, das du awesome findest: \"{what}\"",
 							"shared":	"{user} hat dein geteiltes Foto kommentiert: \"{what}\""
 						}	
-					}	
-				}	
+					},
+					"post": {
+						"you": {
+							"own": "{user} hat deinen Status kommentiert: \"{what}\"",
+							"commented": "{user} hat einen Status von {owner} kommentiert, den du auch kommentiert hast: \"{what}\"",
+							"awesomed":	"{user} hat einen Status von {owner} kommentiert, den du awesome findest: \"{what}\"",
+							"shared":	"{user} hat deinen geteilten Status kommentiert: \"{what}\""
+						}
+					},
+					"wallpost": {
+						"you": {
+							"own": "{user} hat deinen Pinnwandeintrag kommentiert: \"{what}\"",
+							"commented": "{user} hat einen Pinnwandeintrag von {owner} kommentiert, den du auch kommentiert hast: \"{what}\"",
+							"awesomed":	"{user} hat einen Pinnwandeintrag von {owner} kommentiert, den du awesome findest: \"{what}\"",
+							"shared":	"{user} hat deinen geteilten Pinnwandeintrag kommentiert: \"{what}\""
+						}
+					}
+				},
+				"awesome": {
+					"photo": {
+						"you": {
+							"own": "{user} findet dein Foto awesome!",
+							"commented": "{user} findet ein Foto von {owner} awesome, das du kommentiert hast!",
+							"awesomed":	"{user} findet ein Foto von {owner} awesome, das du auch awesome findest!",
+							"shared":	"{user} findet dein geteiltes Foto awesome!"
+						}	
+					},
+					"post": {
+						"you": {
+							"own": "{user} findet deinen Status awesome!",
+							"commented": "{user} findet einen Status von {owner} awesome, den du kommentiert hast!",
+							"awesomed":	"{user} findet einen Status von {owner} awesome, den du auch awesome findest!",
+							"shared":	"{user} findet deinen geteilten Status awesome!"
+						}
+					},
+					"wallpost": {
+						"you": {
+							"own": "{user} findet deinen Pinnwandeintrag awesome!",
+							"commented": "{user} findet einen Pinnwandeintrag von {owner} awesome, den du kommentiert hast!",
+							"awesomed":	"{user} findet einen Pinnwandeintrag von {owner} awesome, den du auch awesome findest!",
+							"shared":	"{user} findet deinen geteilten Pinnwandeintrag awesome!"
+						}
+					},
+						
+					"comment": {
+						"you": {
+							"own": "{user} findet deinen Kommentar awesome!",
+							"commented": "{user} findet einen Kommentar von {owner} awesome, den du kommentiert hast!",
+							"awesomed":	"{user} findet einen Kommentar von {owner} awesome, den du auch awesome findest!",
+						}
+					}
+				},
+				"share": {
+					"photo": {
+						"you": {
+							"own": "{user} hat dein Foto geteilt!"
+							"commented": "{user} hat ein Foto von {owner} geteilt, das du kommentiert hast!",
+							"awesomed":	"{user} hat ein Foto von {owner} geteilt, das du awesome findest!",
+							"shared":	"{user} hat dein geteiltes Foto geteilt!"
+						}	
+					},
+					"post": {
+						"you": {
+							"own": "{user} hat deinen Status geteilt!"
+							"commented": "{user} hat einen Status von {owner} geteilt, den du kommentiert hast!",
+							"awesomed":	"{user} hat einen Status von {owner} geteilt, den du awesome findest!",
+							"shared":	"{user} hat deinen geteilten Status geteilt!"
+						}
+					},
+					"wallpost": {
+						"you": {
+							"own": "{user} hat deinen Pinnwandeintrag geteilt!"
+							"commented": "{user} hat einen Pinnwandeintrag von {owner} geteilt, den du kommentiert hast!",
+							"awesomed":	"{user} hat einen Pinnwandeintrag von {owner} geteilt, den du awesome findest!",
+							"shared":	"{user} hat deinen geteilten Pinnwandeintrag geteilt!"
+						}
+					},
+					"link": {
+						"you": {
+							"own": "{user} hat deinen Link geteilt!"
+							"commented": "{user} hat einen Link von {owner} geteilt, den du kommentiert hast!",
+							"awesomed":	"{user} hat einen Link von {owner} geteilt, den du awesome findest!",
+							"shared":	"{user} hat deinen geteilten Link geteilt!"
+						}
+					}
+				},
+				"whatever": {
+					"photo": {
+						"you": {
+							"own": "{user} hat bei deinem Foto auf whatever geklickt.",
+							"shared":	"{user} hat bei deinem geteilten Foto auf whatever geklickt."
+						}	
+					},
+					"post": {
+						"you": {
+							"own": "{user} hat bei deinem Status auf whatever geklickt.",
+							"shared":	"{user} hat bei deinem geteilten Status auf whatever geklickt."
+						}
+					},
+					"wallpost": {
+						"you": {
+							"own": "{user} hat bei deinem Pinnwandeintrag auf whatever geklickt.",
+							"shared":	"{user} hat bei deinem geteilten Pinnwandeintrag auf whatever geklickt."
+						}
+					},
+					"link": {
+						"you": {
+							"own": "{user} hat bei deinem Link auf whatever geklickt.",
+							"shared":	"{user} hat bei deinem geteilten Link auf whatever geklickt."
+						}
+					},
+					"comment": {
+						"you": {
+							"own": "{user} hat bei deinem Kommentar auf whatever geklickt.",
+						}
+					}
+				},
+				"newfriend": {
+					"you": {
+						"own": "{user} ist jetzt mit dir befreundet!"
+					}
+				},
+				"posted": {	
+					"photo": {
+						"you": {
+							"own": "{user} hat ein Foto an deine Pinnwand gepostet!",
+							"linked": "{user} hat ein Foto gepostet, auf dem du verlinkt bist!"							
+						}
+					},
+					"post": {
+						"you": {
+							"linked": "{user} hat einen Status gepostet, in dem du verlinkt bist!"
+						}
+					},
+					"wallpost": { 
+						"you": {
+							"own": "{user} hat an deine Pinnwand geschrieben!",
+							"linked": "{user} hat einen Pinnwandeintrag geschrieben, auf dem du verlinkt bist!"							
+						}
+					},
+					"comment": {
+						"you": {
+							"linked": "{user} hat einen Kommentar geschrieben, in dem du verlinkt bist!"
+						}
+					}
+				}
+				}
 			}
 		},
 		"settings": {
