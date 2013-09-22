f4b74a68ca81347264121bd01fa88ab2048c2446
Nils Kenneweg <software@ovt.me>
2013-09-22 21:14:59 +0200

    key selection stuff.

--- js/directives/search.js
+++ js/directives/search.js
@@ -1,7 +1,7 @@
 define(["step", "whispeerHelper"], function (step, h) {
 	"use strict";
 
-	function searchDirective(userService) {
+	function searchDirective(userService, $location) {
 		return {
 			transclude: false,
 			scope:	{},
@@ -16,10 +16,13 @@ define(["step", "whispeerHelper"], function (step, h) {
 				scope.clicked = false;
 				scope.query = "";
 				scope.searching = false;
+				scope.empty = false;
+				scope.current = 0;
 
 				var timer = null;
 
 				scope.queryChange = function () {
+					scope.current = 0;
 					if (scope.query.length > 3) {
 						scope.searching = true;
 						window.clearTimeout(timer);
@@ -28,6 +31,8 @@ define(["step", "whispeerHelper"], function (step, h) {
 							step(function () {
 								userService.query(scope.query, this);
 							}, h.sF(function (user) {
+								scope.empty = (user.length === 0);
+
 								theUsers = user;
 								var i;
 								for (i = 0; i < user.length; i += 1) {
@@ -53,6 +58,69 @@ define(["step", "whispeerHelper"], function (step, h) {
 						}, 250);
 					} else {
 						scope.users = [];
+					}
+				};
+
+				scope.currentClass = function (i) {
+					if (i === scope.current) {
+						return "active";
+					}
+
+					return "";
+				};
+
+				function addCurrent(val) {
+					setCurrent(scope.current + val);
+				}
+
+				function setCurrent(val) {
+					scope.current = val;
+
+					if (scope.current < 0) {
+						scope.current = 0;
+					}
+
+					if (scope.current > scope.users.length - 1) {
+						scope.current = scope.users.length - 1;
+					}
+				}
+
+				function selectUser(index) {
+					$location.path(scope.users[index].url);
+
+					scope.focused = false;
+					scope.clicked = false;
+				}
+
+				var END = 35;
+				var HOME = 36;
+				var UP = [38, 33];
+				var DOWN = [40, 32, 34];
+				var ENTER = [13];
+
+				// left: 37, up: 38, right: 39, down: 40,
+				// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+				var keys = UP.concat(DOWN).concat([END, HOME]);
+
+				scope.keydown = function (e) {
+					if (UP.indexOf(e.keyCode) > -1) {
+						addCurrent(-1);
+					}
+
+					if (DOWN.indexOf(e.keyCode) > -1) {
+						addCurrent(1);
+					}
+
+					if (e.keyCode == HOME) {
+						setCurrent(0);
+					}
+
+					if (e.keyCode == ENTER) {
+						selectUser(scope.current);
+					}
+
+					if (keys.indexOf(e.keyCode) > -1) {
+						e.preventDefault();
 					}
 				};
 
@@ -95,15 +163,13 @@ define(["step", "whispeerHelper"], function (step, h) {
 						"location":	"Spenge",
 						"age":	"19",
 						"image":	"/img/profil.jpg"
-					},
-					{
+					},{
 						"name":	"Jacqueline Thenhausen",
 						"mutuals":	"20",
 						"location":	"Spenge",
 						"age":	"23",
 						"image":	"/img/profil.jpg"
-					},
-					{
+					},{
 						"name":	"Svenja Kenneweg",
 						"mutuals":	"220",
 						"location":	"Enger",
