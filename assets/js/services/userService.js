4aa7bbbab8d6724871b1b6dd28356ee146d17063
Daniel Melchior <d.melchior@me.com>
2013-10-08 01:06:14 +0200

    image clean up

--- assets/js/services/userService.js
+++ assets/js/services/userService.js
@@ -114,7 +114,7 @@ define(["step", "whispeerHelper"], function (step, h) {
 					if (image) {
 						this.ne(image);
 					} else {
-						this.ne("/assets/img/profil.jpg");
+						this.ne("/assets/img/user.png");
 					}
 				}), cb);
 			};
