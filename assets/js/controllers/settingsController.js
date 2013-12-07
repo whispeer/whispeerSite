467a1d1c5050e3e405fad1ab5d78665fdfccd46d
Daniel Melchior <d.melchior@me.com>
2013-12-08 00:49:34 +0100

    settings restyle

--- assets/js/controllers/settingsController.js
+++ assets/js/controllers/settingsController.js
@@ -7,6 +7,46 @@ define([], function () {
 
 	function settingsController($scope, cssService) {
 		cssService.setClass("settingsView");
+		$scope.safety = {
+			"name": {
+				first: {
+					encrypt: false,
+					visibility: []
+				},
+				last: {
+					encrypt: true,
+					visibility: []
+				}
+			},
+			"location": {
+				encrypt: true,
+				visibility: []
+			},
+			"birthday": {
+				encrypt: true,
+				visibility: []
+			},
+			"relationship": {
+				encrypt: true,
+				visibility: []
+			},
+			"education": {
+				encrypt: true,
+				visibility: []
+			},
+			"work": {
+				encrypt: true,
+				visibility: []
+			},
+			"gender": {
+				encrypt: true,
+				visibility: []
+			},
+			"languages": {
+				encrypt: true,
+				visibility: []
+			}
+		};
 	}
 
 	settingsController.$inject = ["$scope", "ssn.cssService"];
