bae4b8592ddf52982d6fdf5f48d0dc2a4e264715
Nils Kenneweg <software@ovt.me>
2014-02-27 21:07:37 +0100

    more function wrappers.

--- assets/js/directives/basicDirectives.js
+++ assets/js/directives/basicDirectives.js
@@ -2,27 +2,25 @@
 * BaseDirective
 **/
 
-var directives = ["eatClick", "blur", "onblur", "friend", "strgEnter", "enter", "onfocus", "comment", "post", "scroll", "file", "stopEvent", "inview", "syntaxify", "basicsearch", "autofocus"];
-
-var includes = ["angular"];
-
-var i;
-for (i = 0; i < directives.length; i += 1) {
-	includes.push("directives/" + directives[i]);
-}
-
-console.log(JSON.stringify(includes));
-
-define(includes, function (angular) {
+(function () {
 	"use strict";
+	var directives = ["eatClick", "blur", "onblur", "friend", "strgEnter", "enter", "onfocus", "comment", "post", "scroll", "file", "stopEvent", "inview", "syntaxify", "basicsearch", "autofocus"];
 
-	var d = angular.module("ssn.directives",[]);
+	var includes = ["angular"];
 
 	var i;
 	for (i = 0; i < directives.length; i += 1) {
-		d.directive(directives[i], arguments[i+1]);
+		includes.push("directives/" + directives[i]);
 	}
 
-	return d;
-});
+	define(includes, function (angular) {
+		var d = angular.module("ssn.directives",[]);
 
+		var i;
+		for (i = 0; i < directives.length; i += 1) {
+			d.directive(directives[i], arguments[i+1]);
+		}
+
+		return d;
+	});
+})();
