b74ced391db6b38c1cb0ade4506ba8a07976c9cc
Nils Kenneweg <software@ovt.me>
2014-03-03 17:52:51 +0100

    syntaxify fixed for firefox and refactored.

--- assets/js/directives/syntaxify.js
+++ assets/js/directives/syntaxify.js
@@ -1,32 +1,24 @@
 define([], function () {
 	"use strict";
 
-	return function () {
-
+	return function ($timeout) {
 		return {
-			compile: function () {
-				return {
-					post: function (scope, elm) {
-						var run = false;
-						elm.bind("DOMSubtreeModified", function () {
-							if (!run) {
-								run = true;
-								var text = elm.text();
-								var parts = text.split(/\r\n|\n\r|\r|\n/);
-								var br;
+			restrict: "A",
+			link: function (scope, elm) {
+				$timeout(function () {
+					var text = elm.text();
+					var parts = text.split(/\r\n|\n\r|\r|\n/);
+					var br;
 
-								elm.html("");
+					elm.html("");
 
-								var i;
-								for (i = 0; i < parts.length; i += 1) {
-									br = jQuery("<br>");
-									elm.append(document.createTextNode(parts[i]));
-									elm.append(br);
-								}
-							}
-						});
+					var i;
+					for (i = 0; i < parts.length; i += 1) {
+						br = jQuery("<br>");
+						elm.append(document.createTextNode(parts[i]));
+						elm.append(br);
 					}
-				};
+				});
 			}
 		};
 	};
