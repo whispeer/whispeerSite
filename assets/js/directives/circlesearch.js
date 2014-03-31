9d7b234a77ba4841dd18c734a329e0db2c034bf9
Nils Kenneweg <nk@ovt.me>
2014-03-31 11:38:25 +0200

    create circles directly from user.

--- assets/js/directives/circlesearch.js
+++ assets/js/directives/circlesearch.js
@@ -9,12 +9,25 @@ define(["step", "whispeerHelper"], function (step, h) {
 			templateUrl: "/assets/views/directives/circleSearch.html",
 			replace: true,
 			link: function postLink(scope, element, attrs) {
+				var user;
+				scope.$on("createCircle", function (event, data) {
+					if (user) {
+						circleService.create(data, h.sF(function () {
+							var circles = circleService.inWhichCircles(user);
+							scope.$broadcast("resetSearch");
+							scope.$broadcast("initialSelection", circles.map(function (e) {
+								return e.data;
+							}));
+						}), [user])
+					}
+				});
+
 				function submitResults(results) {
 					scope.$broadcast("queryResults", results);
 				}
 
 				if (attrs["user"]) {
-					var user = h.parseDecimal(scope.$parent.$eval(attrs["user"]));
+					user = h.parseDecimal(scope.$parent.$eval(attrs["user"]));
 					step(function () {
 						circleService.loadAll(this);
 					}, h.sF(function () {
