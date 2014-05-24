7634bccdf7abe67d98b7c33785567612f9256024
Nils Kenneweg <software@ovt.me>
2014-05-24 17:16:24 +0200

    remove debugger. fix bug.

--- assets/js/migrations/00002-imageBlobMigration.js
+++ assets/js/migrations/00002-imageBlobMigration.js
@@ -4,32 +4,41 @@ define(["step", "whispeerHelper"], function (step, h) {
 		var userService = $injector.get("ssn.userService");
 		var blobService = $injector.get("ssn.blobService");
 
-		var newSettings;
-
-		step(function () {
-			settingsService.getBranch("privacy", this);
-		}, h.sF(function (privacySettings) {
-			if (!privacySettings["imageBlob"]) {
+		function setImageBlobPrivacySetting(privacySettings, cb) {
+			var newSettings;
+			step(function () {
 				//set image privacy settings to public
 				newSettings = h.deepCopyObj(privacySettings);
 				newSettings.imageBlob = {
 					encrypt: false,
 					visibility: []
-				};				
-			}
+				};
 
+				settingsService.updateBranch("privacy", newSettings, this);
+			}, h.sF(function () {
+				settingsService.uploadChangedData(this);
+			}), cb);
+		}
+
+		step(function () {
+			settingsService.getBranch("privacy", this);
+		}, h.sF(function (privacySettings) {
+			if (!privacySettings.imageBlob) {
+				setImageBlobPrivacySetting(privacySettings, this);
+			} else {
+				this.ne();
+			}
+		}), h.sF(function () {
 			userService.getown().getProfileAttribute("image", this);
 		}), h.sF(function (imageData) {
 			if (imageData) {
 				var myBlob = blobService.createBlob(h.dataURItoBlob(imageData));
 
-				debugger;
-
 				this.parallel.unflatten();
 				myBlob.upload(this.parallel());
 				myBlob.getHash(this.parallel());
 			} else {
-				this.ne();
+				this.last.ne();
 			}
 		}), h.sF(function (blobid, hash) {
 			if (blobid) {
@@ -39,12 +48,11 @@ define(["step", "whispeerHelper"], function (step, h) {
 				}, this.parallel());
 
 				userService.getown().setProfileAttribute("image", "", this.parallel());
-			}
-
-			settingsService.updateBranch("privacy", newSettings, this.parallel());
+			} else {
+				this.ne();
+			}			
 		}), h.sF(function () {
-			userService.getown().uploadChangedProfile(this.parallel());
-			settingsService.uploadChangedData(this.parallel());
+			userService.getown().uploadChangedProfile(this);
 		}), cb);
 	};
 });
