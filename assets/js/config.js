45faddf67c6a7e612e96982e1bffd92590976af6
Nils Kenneweg <nk@ovt.me>
2014-06-03 12:27:16 +0200

    use only relative links.

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,18 @@
+var useServer = true;
+
 var config = {
 	https: false,
 	ws: "127.0.0.1",
 	wsPort: 3000
 };
+
+if (useServer) {
+	config = {
+		https: true,
+		ws: "beta.whispeer.de",
+		wsPort: 3001
+	};
+}
 
 if (typeof module != "undefined" && module.exports) {
     module.exports = config;
