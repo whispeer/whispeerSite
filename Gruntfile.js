1116ceb72938e62436d84e818a8794949c8f1a54
Nils Kenneweg <software@ovt.me>
2014-06-09 18:35:17 +0200

    indent using tabs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,12 @@ var grunt = require("grunt");
 grunt.loadNpmTasks('grunt-contrib-jshint');
 
 grunt.initConfig({
-  jshint: {
-    all: {
-    	src: ['Gruntfile.js', 'assets/js/**/*.js'],
-    	options: {
-    		jshintrc: true
-    	}
-    }
-  }
+	jshint: {
+		all: {
+			src: ['Gruntfile.js', 'assets/js/**/*.js'],
+			options: {
+				jshintrc: true
+			}
+		}
+	}
 });
