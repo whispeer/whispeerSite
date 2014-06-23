f6ae1c23e649850575a0b3a41ebb68d392db6269
Martin Czuchra <martin.czuchra@googlemail.com>
2014-06-23 17:27:46 +0200

    Compiles less automatically.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 var grunt = require("grunt");
 grunt.loadNpmTasks("grunt-contrib-jshint");
+grunt.loadNpmTasks("grunt-contrib-less");
+grunt.loadNpmTasks("grunt-contrib-watch");
 
 grunt.initConfig({
 	jshint: {
@@ -7,6 +9,25 @@ grunt.initConfig({
 			src: ["Gruntfile.js", "assets/js/**/*.js"],
 			options: {
 				jshintrc: true
+			}
+		}
+	},
+	less: {
+		development: {
+			options: {
+				paths: ["assets/less"]
+			},
+			files: {
+				"assets/css/style.css": "assets/less/style.less"
+			}
+		}
+	},
+	watch: {
+		scripts: {
+			files: ["assets/less/**/*.less"],
+			tasks: ["less"],
+			options: {
+				spawn: false
 			}
 		}
 	}
