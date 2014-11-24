3b328895eadbb7e3e943cc25f9539a44a54816f4
Daniel Melchior <d.melchior@me.com>
2014-11-24 17:58:33 +0100

    apply now box class to circleView

--- assets/js/controllers/circlesController.js
+++ assets/js/controllers/circlesController.js
@@ -8,18 +8,18 @@ define(["whispeerHelper"], function (h) {
 	function circlesController($scope, cssService, circleService, errorService, localize) {
 		$scope.circleid = 0;
 		$scope.showCircle = !$scope.mobile;
-		
+
 		$scope.editingTitle = {
 			"success":		true,
 			"failure":		false,
 			"operation":	false,
 			"active":		false
 		};
-		
+
 		$scope.editTitle = function () {
 			$scope.editingTitle.active = true;
 		};
-		
+
 		$scope.saveTitle = function () {
 			$scope.editingTitle.success = true;
 			$scope.editingTitle.active = false;
@@ -29,8 +29,8 @@ define(["whispeerHelper"], function (h) {
 
 		$scope.circles = circleService.data.circles;
 
-		cssService.setClass("circlesView");
-		
+		cssService.setClass("circlesView", true);
+
 		$scope.getLength = function(obj) {
 			return obj.length;
 		};
@@ -55,7 +55,7 @@ define(["whispeerHelper"], function (h) {
 
 			$scope.showCircle = !$scope.mobile;
 		};
-		
+
 		$scope.removeCircle = function () {
 			var response = confirm(localize.getLocalizedString("views.circles.removeCircle"));
 			if (response) {
@@ -76,7 +76,7 @@ define(["whispeerHelper"], function (h) {
 			$scope.thisCircle = {};
 			$scope.circleid = 0;
 		};
-		
+
 		$scope.unloadCircleMobile = function () {
 			$scope.showCircle = false;
 
