3ae18b80b3b80903c1cc1b6dabade638fcfd14d7
Nils Kenneweg <nk@ovt.me>
2014-12-06 15:31:52 +0100

    migration example aded.

---
+++ assets/js/migrations/migrationExample.js
@@ -0,0 +1,9 @@
+define(["step", "whispeerHelper"], function (step, h) {
+	"use strict";
+	return function ($injector, cb) {
+		step(function () {
+		}, h.sF(function () {
+			this.ne(true);
+		}), cb);
+	};
+});
