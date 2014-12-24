8cca000309f0b31443d38466d4472d3b85ccbb4e
Nils Kenneweg <nk@ovt.me>
2014-12-24 12:49:20 +0100

    directive for accessible non-button clicks.

--- assets/js/directives/basicDirectives.js
+++ assets/js/directives/basicDirectives.js
@@ -4,7 +4,7 @@
 
 (function () {
 	"use strict";
-	var directives = ["smoothScroll", "eatClick", "blur", "onblur", "person", "strgEnter", "enter", "onfocus", "comment", "post", "scroll", "file", "stopEvent", "inview", "syntaxify", "basicsearch", "autofocus", "errorHint", "savebutton", "advancedsend", "focusMe", "userimage"];
+	var directives = ["smoothScroll", "eatClick", "blur", "onblur", "person", "strgEnter", "enter", "onfocus", "comment", "post", "scroll", "file", "stopEvent", "inview", "syntaxify", "basicsearch", "autofocus", "errorHint", "savebutton", "advancedsend", "focusMe", "userimage", "accessibleClick"];
 
 	var includes = ["angular"];
 
