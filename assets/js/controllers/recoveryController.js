516c8c89ac0e3409b77a5e6283833bbac7c9d483
Nils Kenneweg <nk@ovt.me>
2015-01-19 13:04:28 +0100

    all input methods for the qr-code work now!

--- assets/js/controllers/recoveryController.js
+++ assets/js/controllers/recoveryController.js
@@ -1,4 +1,4 @@
-define(["whispeerHelper", "step", "asset/state"], function (h, step, State) {
+define(["whispeerHelper", "step", "asset/state", "libs/qrreader"], function (h, step, State, qrreader) {
 	"use strict";
 
 	function recoveryController($scope, $rootScope, $routeParams, socketService, keyStore, sessionService, userService, cssService, errorService) {
@@ -39,7 +39,7 @@ define(["whispeerHelper", "step", "asset/state"], function (h, step, State) {
 			}, errorService.failOnError(savePasswordState));
 		};
 
-		$scope.backupKeyCallback = function (key) {
+		function doRecovery(key, cb) {
 			step(function () {
 				keyStore.setKeyGenIdentifier($routeParams.nick);
 				var keyID = keyStore.sym.loadBackupKey(keyStore.format.unBase32(key));
@@ -51,8 +51,36 @@ define(["whispeerHelper", "step", "asset/state"], function (h, step, State) {
 			}, h.sF(function () {
 				$scope.changePassword = true;
 				$rootScope.$broadcast("ssn.recovery");
-			}), function (e) {
-				$scope.qr.reset();
+				this.ne();
+			}), cb);
+		}
+
+		$scope.fileUpload = function (e) {
+			step(function () {
+				var file = e.target.files[0];
+				if (!file.type.match(/image.*/i)) {
+					return;
+				}
+
+				qrreader.decode(h.toUrl(file), this);
+			}, function (code) {
+				doRecovery(code, errorService.criticalError);
+			});
+		};
+
+		$scope.loadBackupKeyManual = function () {
+			loadBackupKeyState.pending();
+
+			doRecovery($scope.manualCode, errorService.failOnError(loadBackupKeyState));
+		};
+
+		$scope.backupKeyCallback = function (key) {
+			step(function () {
+				doRecovery(key, this);
+			}, function (e) {
+				if (e) {
+					$scope.qr.reset();
+				}
 				this(e);
 			}, errorService.criticalError);
 		};
