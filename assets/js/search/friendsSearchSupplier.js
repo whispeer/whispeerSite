7721cf6ff9dedb7c9080319c1184b9e53909e1f3
Nils Kenneweg <nk@ovt.me>
2015-04-15 17:57:16 +0200

    do not request search results for less than 3 letters.

--- assets/js/search/friendsSearchSupplier.js
+++ assets/js/search/friendsSearchSupplier.js
@@ -1,14 +1,18 @@
-define(["angular", "bluebird"], function (angular, Promise) {
+define(["angular", "bluebird"], function (angular, Bluebird) {
 	"use strict";
 	return function () {
 		angular.module("ssn.search").factory("friendsSearchSupplier", ["ssn.userService", function (userService) {
 			var Search = function () {};
 
 			Search.prototype.search = function (query) {
-				var action = Promise.promisify(userService.queryFriends, userService);
+				if (query.length < 3) {
+					return Bluebird.reject("minimum3letters");
+				}
+
+				var action = Bluebird.promisify(userService.queryFriends, userService);
 
 				return action(query).bind(this).map(function (user) {
-					var loadBasicData = Promise.promisify(user.loadBasicData, user);
+					var loadBasicData = Bluebird.promisify(user.loadBasicData, user);
 					return loadBasicData().then(function () {
 						return user;
 					});
