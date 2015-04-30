2abd84ff5183067a1852660eaa9f3689f7fd8b9e
Nils Kenneweg <nk@ovt.me>
2015-04-30 17:02:51 +0200

    add login method.

--- assets/js/login/loginDataService.js
+++ assets/js/login/loginDataService.js
@@ -14,8 +14,51 @@ define(["step", "asset/state", "login/loginModule"], function (step, State, logi
 			failure: false,
 			failedOnce: false,
 			isHeaderForm: false,
+
+			loginServer: function (name, password, callback) {
+				step(function loginStartup() {
+					socketService.emit("session.token", {
+						identifier: name
+					}, this);
+				}, function hashWithToken(e, data) {
+					if (e) {
+						this.last({ unknownName: true });
+					} else {
+						if (data.salt.length !== 16) {
+							throw new SecurityError("server wut?");
+						}
+
+						var hash = keyStoreService.hash.hashPW(password, data.salt);
+
+						hash = keyStoreService.hash.hash(hash + data.token);
+						socketService.emit("session.login", {
+							identifier: name,
+							password: hash,
+							token: data.token
+						}, this);
+					}
+				}, function loginResults(e, data) {
+					if (e) {
+						this.last({ wrongPassword: true });
+					} else {
+						sessionHelper.resetKey();
+						
+						sessionService.setSID(data.sid, data.userid);
+						keyStoreService.security.setPassword(password);
+
+						this.last.ne();
+					}
+				}, callback);
+			},
+
 			login: function () {
 				loginState.pending();
+
+				res.failure = {};
+				res.failure.UNKNOWNNAME = 0;
+				res.failure.WRONGPASSWORD = 1;
+				res.failure.NOCONNECTION = 2;
+				res.failure.UNKNOWN = 5;
 
 				res.success = false;
 				res.unknownName = false;
@@ -27,6 +70,9 @@ define(["step", "asset/state", "login/loginModule"], function (step, State, logi
 				}, function (e) {
 					if (e) {
 						loginState.failed();
+
+						/* write username and failure reason to localstorage */
+						/* redirect to loginDetailed.html (except if we are there) */
 						res.failedOnce = true;
 						if (e.wrongPassword) {
 							res.wrongPassword = true;
@@ -36,11 +82,12 @@ define(["step", "asset/state", "login/loginModule"], function (step, State, logi
 							res.failure = true;
 						}
 
-						if ($location.path() !== "/login" && !$rootScope.$$childHead.mobile) {
-							$location.path("/login");
-						}
+						window.top.location = "loginDetail.html";
 					} else {
 						loginState.success();
+
+						window.location.href = "/main";
+
 						res.failedOnce = false;
 						res.identifier = "";
 						res.password = "";
@@ -59,7 +106,7 @@ define(["step", "asset/state", "login/loginModule"], function (step, State, logi
 		return res;
 	};
 
-	service.$inject = ["$rootScope", "$location", "ssn.sessionHelper"];
+	service.$inject = ["$rootScope", "$location"];
 
 	loginModule.factory("ssn.loginDataService", service);
 });
