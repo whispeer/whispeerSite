3b7e8918627827e777e553595626966441c063ce
Nils Kenneweg <nk@ovt.me>
2015-05-05 23:13:34 +0200

    lazyiframe does not need any timeout.

--- assets/js/directives/lazyiframe.js
+++ assets/js/directives/lazyiframe.js
@@ -6,24 +6,22 @@ define(["directives/directivesModule"], function (directivesModule) {
 			restrict: "E",
 			template: "",
 			link: function (scope, iElement, iAttrs) {
-				window.setTimeout(function () {
-					var attributes = {};
+				var attributes = {};
 
-					Object.keys(iAttrs.$attr).filter(function (attr) {
-						return attr.indexOf("ng") !== 0;
-					}).map(function (attr) {
-						return {
-							key: attr,
-							val: iAttrs.$attr[attr]
-						};
-					}).forEach(function (attr) {
-						attributes[attr.val] = iAttrs[attr.key];
-					});
+				Object.keys(iAttrs.$attr).filter(function (attr) {
+					return attr.indexOf("ng") !== 0;
+				}).map(function (attr) {
+					return {
+						key: attr,
+						val: iAttrs.$attr[attr]
+					};
+				}).forEach(function (attr) {
+					attributes[attr.val] = iAttrs[attr.key];
+				});
 
-					iElement.append(
-						jQuery("<iframe>").attr(attributes)
-					);
-				}, 5);
+				iElement.append(
+					jQuery("<iframe>").attr(attributes)
+				);
 			}
 		};
 	}
