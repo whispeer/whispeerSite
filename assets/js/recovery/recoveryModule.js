7f41ae788bb1032220f16e41d6ccffde8ea8743f
Nils Kenneweg <nk@ovt.me>
2015-05-11 13:27:20 +0200

    make recovery locale-aware.

--- assets/js/recovery/recoveryModule.js
+++ assets/js/recovery/recoveryModule.js
@@ -1,4 +1,4 @@
-define(["angular"], function (angular) {
+define(["angular", "config/localizationConfig"], function (angular) {
 	"use strict";
-	return angular.module("ssn.recovery", ["ssn.services", "ssn.directives", "ssn.models", "ssn.interceptors.config", "localization"]);
+	return angular.module("ssn.recovery", ["ssn.services", "ssn.directives", "ssn.models", "ssn.interceptors.config", "ssn.locale.config", "localization"]);
 });
