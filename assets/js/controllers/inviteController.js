21a79c3e8f065c94903f2a5b141216ff7ba05a7b
Nils Kenneweg <nk@ovt.me>
2015-05-23 23:28:49 +0200

    remove outdated code.

--- assets/js/controllers/inviteController.js
+++ assets/js/controllers/inviteController.js
@@ -5,35 +5,16 @@
 define(["step", "whispeerHelper", "asset/state", "controllers/controllerModule"], function (step, h, State, controllerModule) {
 	"use strict";
 
-	function inviteController($scope, $location, cssService, keyStore, socketService, errorService) {
+	function inviteController($scope, $location, cssService) {
 		cssService.setClass("inviteView");
-
-		var inviteGenerateState = new State();
-		$scope.inviteGenerateState = inviteGenerateState.data;
 
 		$scope.domain = $location.protocol() + "://" + $location.host();
 		$scope.url = encodeURIComponent($scope.domain);
 		$scope.text = "whispeer is an awesome social network!";
 		$scope.hashtags = "privacy,whispeer";
-
-		$scope.empty = function (val) {
-			return val === "" || !h.isset(val);
-		};
-
-		$scope.generateInvite = function () {
-			inviteGenerateState.pending();
-
-			step(function () {
-				socketService.emit("invites.generateCode", {}, this);
-			}, h.sF(function (result) {
-				$scope.inviteCode = result.inviteCode;
-
-				this.ne();
-			}), errorService.failOnError(inviteGenerateState));
-		};
 	}
 
-	inviteController.$inject = ["$scope", "$location", "ssn.cssService", "ssn.keyStoreService", "ssn.socketService", "ssn.errorService"];
+	inviteController.$inject = ["$scope", "$location", "ssn.cssService"];
 
 	controllerModule.controller("ssn.inviteController", inviteController);
 });
