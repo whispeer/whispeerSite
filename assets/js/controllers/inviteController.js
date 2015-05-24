f2ad73d7b8706713dc2835e27e4c29b22daf4659
Nils Kenneweg <nk@ovt.me>
2015-05-24 11:08:26 +0200

    translate share text / hashtags.

--- assets/js/controllers/inviteController.js
+++ assets/js/controllers/inviteController.js
@@ -5,16 +5,16 @@
 define(["step", "whispeerHelper", "asset/state", "controllers/controllerModule"], function (step, h, State, controllerModule) {
 	"use strict";
 
-	function inviteController($scope, $location, cssService) {
+	function inviteController($scope, $location, cssService, localize) {
 		cssService.setClass("inviteView");
 
 		$scope.domain = $location.protocol() + "://" + $location.host();
 		$scope.url = encodeURIComponent($scope.domain);
-		$scope.text = "whispeer is an awesome social network!";
-		$scope.hashtags = "privacy,whispeer";
+		$scope.text = localize.getLocalizedString("views.invite.shareText", {});
+		$scope.hashtags = localize.getLocalizedString("views.invite.shareHashTags", {});
 	}
 
-	inviteController.$inject = ["$scope", "$location", "ssn.cssService"];
+	inviteController.$inject = ["$scope", "$location", "ssn.cssService", "localize"];
 
 	controllerModule.controller("ssn.inviteController", inviteController);
 });
