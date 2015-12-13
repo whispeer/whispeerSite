943c8c23877cb24475cbf84d5537a5235de452ae
Nils Kenneweg <nk@ovt.me>
2015-12-13 15:28:45 +0100

    copy app. copy build assets to fallback dir.

--- scripts/deploy_whispeer.sh
+++ scripts/deploy_whispeer.sh
@@ -10,14 +10,18 @@ rm -rf whispeer-deploy
 cp -r whispeer whispeer-deploy
 cp config.js ./whispeer-deploy/assets/js/
 cd whispeer-deploy
+
 grunt build:production
 
-
 cd /var/www/
+sudo cp whispeer/assets/js/build/* whispeer-build/
+
 sudo rm -rf whispeer
 sudo mkdir whispeer
+
 sudo cp /home/nilos/whispeer-deploy/index.html /var/www/whispeer
 sudo cp -r /home/nilos/whispeer-deploy/assets /var/www/whispeer
 sudo cp -r /home/nilos/whispeer-deploy/static /var/www/whispeer
+sudo cp -r /home/nilos/whispeer-app/versions /var/www/whispeer/app
 #sudo cp /home/nilos/whispeer-deploy/manifest.mf /var/www/whispeer
 
