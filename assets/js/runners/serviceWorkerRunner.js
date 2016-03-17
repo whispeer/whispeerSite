38afdb0352421c1e9efa5871f87757d89394a6e8
Nils Kenneweg <nk@ovt.me>
2016-03-17 20:33:11 +0100

    fix service worker unregister.

--- assets/js/runners/serviceWorkerRunner.js
+++ assets/js/runners/serviceWorkerRunner.js
@@ -4,22 +4,25 @@
 define(["config"], function (config) {
 	"use strict";
 
-		if (config.serviceWorker.enabled && "serviceWorker" in navigator) {
-			navigator.serviceWorker.register("/sw.js").then(function(registration) {
-				console.log("ServiceWorker registration successful with scope: ",    registration.scope);
-			}).catch(function(err) {
-				console.log("ServiceWorker registration failed: ", err);
-			});
+		if ("serviceWorker" in navigator) {
+			if (config.serviceWorker.enabled) {
+				navigator.serviceWorker.register("/sw.js").then(function(registration) {
+					console.info("ServiceWorker registration successful with scope: ",    registration.scope);
+				}).catch(function(err) {
+					console.info("ServiceWorker registration failed: ", err);
+				});
+			} else {
+				navigator.serviceWorker.getRegistration("/").then(function(registration) {
+					if (registration) {
+						return registration.unregister();
+					}
+				}).then(function (unregistered) {
+					if (unregistered) {
+						console.info("unregistered service worker");
+					}
+				});
+			}
 		} else {
-			navigator.serviceWorker.getRegistration("/").then(function(registration) {
-				if (registration) {
-					return registration.unregister();
-				}
-			}).then(function (unregistered) {
-				if (unregistered) {
-					console.info("unregistered service worker");
-				}
-			});
-			console.log("Service worker enabled: " + config.serviceWorker.enabled);
+			console.info("Browser does not support service worker");
 		}
 });
