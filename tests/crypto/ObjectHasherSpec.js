85e32e4ad095d725f607ef89235fc5017fd56afb
Nils Kenneweg <nk@ovt.me>
2016-05-01 21:54:54 +0200

    add tests for object hasher version 1.

--- tests/crypto/ObjectHasherSpec.js
+++ tests/crypto/ObjectHasherSpec.js
@@ -59,40 +59,78 @@ define(["crypto/objectHasher"], function (ObjectHasher) {
 	});
 
 	describe("version 3", function () {
-		it ("does not change empty objects", function () {
+		it("does not change empty objects", function () {
 			var result = new ObjectHasher({}, 3).stringify();
 
 			expect(result).toEqual('{}');
 		});
 
-		it ("does not change empty array", function () {
+		it("does not change empty array", function () {
 			var result = new ObjectHasher([], 3).stringify();
 
 			expect(result).toEqual('[]');
 		});
 
-		it ("stringifies values", function () {
+		it("stringifies values", function () {
 			var result = new ObjectHasher({a: 5}, 3).stringify();
 
 			expect(result).toEqual('{"a":"5"}');
 		});
 
-		it ("sorts values", function () {
+		it("sorts values", function () {
 			var result = new ObjectHasher({b: "a", a: "b"}, 3).stringify();
 
 			expect(result).toEqual('{"a":"b","b":"a"}');
 		});
 
-		it ("stringifies sub objects", function () {
+		it("stringifies sub objects", function () {
 			var result = new ObjectHasher({a: {b: "c"}}, 3).stringify();
 
 			expect(result).toEqual('{"a":"{\\"b\\":\\"c\\"}"}');
 		});
 
-		it ("sorts attributes of sub objects", function () {
+		it("sorts attributes of sub objects", function () {
 			var result = new ObjectHasher({a: {b: "c", a: "c"}}, 3).stringify();
 
 			expect(result).toEqual('{"a":"{\\"a\\":\\"c\\",\\"b\\":\\"c\\"}"}');
+		});
+	});
+
+	describe("version 1", function () {
+		it("does not change empty objects", function () {
+			var result = new ObjectHasher({}, 1).stringify();
+
+			expect(result).toEqual('{}');
+		});
+
+		it("does not change empty array", function () {
+			var result = new ObjectHasher([], 1).stringify();
+
+			expect(result).toEqual('[]');
+		});
+
+		it("stringifies values", function () {
+			var result = new ObjectHasher({a: 5}, 1).stringify();
+
+			expect(result).toEqual('{"a":"hash::e2833fab2d34fe77dd528d8b98534420d3fe166aecd550918d6f1109e53a3e25"}');
+		});
+
+		it("sorts values", function () {
+			var result = new ObjectHasher({b: "a", a: "b"}, 1).stringify();
+
+			expect(result).toEqual('{"a":"hash::9905f75ee3631db65809e2415b94df99ffd0fb6711571ab78b93695f043008ba","b":"hash::6216399d51f3581b0f8418644b4fd0b50f86a25b59101afb08dd0785dd4e8d9c"}');
+		});
+
+		it("stringifies sub objects", function () {
+			var result = new ObjectHasher({a: {b: "c"}}, 1).stringify();
+
+			expect(result).toEqual('{"a":"hash::2b9f8de35d456ab77fe22f2e2ab6073acaa964c3cb8684a935dbecb0ef709d07"}');
+		});
+
+		it("sorts attributes of sub objects", function () {
+			var result = new ObjectHasher({a: {b: "c", a: "c"}}, 1).stringify();
+
+			expect(result).toEqual('{"a":"hash::058c781de71fbdb3d86ea546297cfe202f58e54c56e7c2610330368375fc6594"}');
 		});
 	});
 
