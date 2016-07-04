4996259045b6cdce540e07dd85703c3a8713be27
Nils Kenneweg <nk@ovt.me>
2016-07-04 11:51:42 +0200

    $apply where promises are used (user)

--- assets/js/models/user.js
+++ assets/js/models/user.js
@@ -66,7 +66,7 @@ define(["step", "whispeerHelper", "asset/state", "asset/securedDataWithMetaData"
 		return h.arrayUnique(profileTypes);
 	}
 
-	function userModel($injector, blobService, keyStoreService, ProfileService, sessionService, settingsService, socketService, friendsService, errorService) {
+	function userModel($injector, $rootScope, blobService, keyStoreService, ProfileService, sessionService, settingsService, socketService, friendsService, errorService) {
 		return function User (providedData) {
 			var theUser = this, mainKey, signKey, cryptKey, friendShipKey, friendsKey, migrationState, signedKeys, signedOwnKeys;
 			var id, mail, nickname, publicProfile, privateProfiles = [], myProfile, mutualFriends;
@@ -513,7 +513,7 @@ define(["step", "whispeerHelper", "asset/state", "asset/securedDataWithMetaData"
 					sessionService.setPassword(newPassword);
 					this.ne();
 				}), cb);
-			},
+			};
 
 			this.loadFullData = function (cb) {
 				step(function () {
@@ -588,9 +588,13 @@ define(["step", "whispeerHelper", "asset/state", "asset/securedDataWithMetaData"
 						theUser.data.added = friendsService.didIRequest(theUser.getID());
 						theUser.data.isMyFriend = friendsService.areFriends(theUser.getID());
 
+						$rootScope.$apply();
+
 						friendsService.listen(function () {
 							theUser.data.added = friendsService.didIRequest(theUser.getID());
 							theUser.data.isMyFriend = friendsService.areFriends(theUser.getID());
+
+							$rootScope.$apply();
 						});
 					});
 
@@ -598,6 +602,8 @@ define(["step", "whispeerHelper", "asset/state", "asset/securedDataWithMetaData"
 					theUser.data.ignoreFriendState = ignoreFriendState.data;
 
 					theUser.loadImage();
+
+					$rootScope.$apply();
 
 					this.ne();
 				}), cb);
@@ -758,7 +764,7 @@ define(["step", "whispeerHelper", "asset/state", "asset/securedDataWithMetaData"
 		};
 	}
 
-	userModel.$inject = ["$injector", "ssn.blobService",  "ssn.keyStoreService", "ssn.profileService", "ssn.sessionService", "ssn.settingsService", "ssn.socketService", "ssn.friendsService", "ssn.errorService"];
+	userModel.$inject = ["$injector",  "$rootScope", "ssn.blobService",  "ssn.keyStoreService", "ssn.profileService", "ssn.sessionService", "ssn.settingsService", "ssn.socketService", "ssn.friendsService", "ssn.errorService"];
 
 	modelsModule.factory("ssn.models.user", userModel);
 });
