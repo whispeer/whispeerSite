ccac3e2d98c7487f6ac7ef69a3ac4b4216ac11b5
Nils Kenneweg <nk@ovt.me>
2016-07-09 12:52:08 +0200

    disable debug info in register/login.

--- assets/js/login/loginModule.js
+++ assets/js/login/loginModule.js
@@ -1,4 +1,10 @@
-define(["angular", "config/localizationConfig", "directives/savebutton", "directives/mobile", "directives/loadVal", "directives/isIframe", "localizationModule"], function (angular) {
+define(["angular", "config", "config/localizationConfig", "directives/savebutton", "directives/mobile", "directives/loadVal", "directives/isIframe", "localizationModule"], function (angular, config) {
 	"use strict";
-	return angular.module("ssn.login", ["ssn.services", "ssn.directives", "ssn.locale.config", "localization"]);
+	return angular.module("ssn.login", ["ssn.services", "ssn.directives", "ssn.locale.config", "localization"],
+		["$compileProvider", function ($compileProvider) {
+			if (!config.debug) {
+				$compileProvider.debugInfoEnabled(false);
+			}
+		}]
+	);
 });
