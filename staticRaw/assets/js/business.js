d002d030b2d68993db083a4a0db31a7b8b40b265
Daniel Melchior <daniel@whispeer.de>
2016-12-23 17:07:03 +0100

    change form at bottom to newsletter signup. works.

---
+++ staticRaw/assets/js/business.js
@@ -0,0 +1,40 @@
+(function() {
+	"use strict";
+
+	var api = "";
+	var form = document.getElementById("mail_signup");
+
+	form.addEventListener("submit", function(event) {
+		event.preventDefault();
+
+		if(event.srcElement.checkValidity()) {
+			var data = {
+				"username": document.getElementById("signup_name").value,
+				"mail": document.getElementById("signup_mail").value
+			};
+
+			document.getElementById("account_teaser").classList.add("fade-out");
+
+			event.srcElement.classList.add("fade-out");
+
+			document.getElementById("mail_signup-after-send").classList.add("fade-in");
+
+			var req = new XMLHttpRequest();
+
+			req.open("POST", api, true);
+
+			req.onreadystatechange = function() {
+				if(req.readyState != 4 || req.status != 200) return;
+
+				debugger;
+
+				document.getElementById("mail_signup-after-send__loading").classList.add("hide");
+				document.getElementById("mail_signup-after-send__finished").classList.remove("hide");
+			};
+
+			req.send(JSON.stringify(data));
+		}
+
+		return false;
+	});
+})();
