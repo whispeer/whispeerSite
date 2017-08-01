0c5d822a29ee4c67f48d9058522bc721dff8ab78
Nils Kenneweg <nk@ovt.me>
2017-08-01 11:28:17 +0200

    bugfix

--- assets/js/verifyMail/verifyMailModule.js
+++ assets/js/verifyMail/verifyMailModule.js
@@ -3,4 +3,4 @@ const angular = require("angular");
 require("directives/savebutton");
 require("directives/mobile");
 require("runners/promiseRunner");
-module.exports = angular.module("ssn.verifyMail", ["ssn.services", "ssn.directives", "ssn.runners"]);
+module.exports = angular.module("ssn.verifyMail", ["ssn.directives", "ssn.runners"]);
