dbb4d80f190192449da8a605704599381778c5e0
Nils Kenneweg <nk@ovt.me>
2017-09-14 18:06:38 +0200

    new deploy script

--- scripts/deploy_whispeer.sh
+++ scripts/deploy_whispeer.sh
@@ -6,21 +6,40 @@ sudo npm install -g yarn
 
 cd ..
 
-rm -rf whispeer-deploy
-cp -r whispeer whispeer-deploy
-cd whispeer-deploy
+rm -rf whispeer-build
+mkdir whispeer-build
 
-if [ -n "$WHISPEER_BUSINESS" ]; then
-  echo "Building business version"
-  cp b2b/de/index.html staticRaw/de/index.html
-  cp b2b/en/index.html staticRaw/en/index.html
+cd whispeer-build
 
-  cp b2b/grey.svg assets/img/logo/grey.svg
-  cp b2b/white.svg assets/img/logo/white.svg
-  cp b2b/white.svg assets/img/logo/white_darker.svg
-fi
+mkdir b2b
+mkdir b2c
+
+cp -r ../whispeer deploy
+cd deploy
 
 ./build_whispeer.sh
+
+cp ./index.html ../b2c
+cp ./sw.js ../b2c
+cp -r ./assets ../b2c
+cp -r ./static ../b2c
+cp -r ./node_modules/bluebird/js/browser ../b2c/assets/bluebird
+
+echo "Building business version"
+cp b2b/de/index.html staticRaw/de/index.html
+cp b2b/en/index.html staticRaw/en/index.html
+
+cp b2b/grey.svg assets/img/logo/grey.svg
+cp b2b/white.svg assets/img/logo/white.svg
+cp b2b/white.svg assets/img/logo/white_darker.svg
+
+WHISPEER_BUSINESS=true ./build_whispeer.sh
+
+cp ./index.html ../b2b
+cp ./sw.js ../b2b
+cp -r ./assets ../b2b
+cp -r ./static ../b2b
+cp -r ./node_modules/bluebird/js/browser ../b2b/assets/bluebird
 
 cd /var/www/
 sudo cp whispeer/assets/js/build/* whispeer-build/ | true
@@ -30,12 +49,8 @@ sudo mkdir whispeer
 
 sudo ln -s ../whispeer-build whispeer/oldBuild
 
-sudo cp /home/nilos/whispeer-deploy/index.html /var/www/whispeer
-sudo cp /home/nilos/whispeer-deploy/sw.js /var/www/whispeer
-sudo cp -r /home/nilos/whispeer-deploy/assets /var/www/whispeer
-sudo cp -r /home/nilos/whispeer-deploy/static /var/www/whispeer
-
-sudo cp -r /home/nilos/whispeer-deploy/node_modules/bluebird/js/browser /var/www/whispeer/assets/bluebird
+sudo cp -r /home/nilos/whispeer-build/b2b /var/www/whispeer
+sudo cp -r /home/nilos/whispeer-deploy/b2c /var/www/whispeer
 
 if [ -d "/home/nilos/whispeer-app" ]; then
   sudo cp -r /home/nilos/whispeer-app/versions /var/www/whispeer/app
