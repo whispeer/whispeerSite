02efc09da3ad3e5bc6fd897848999d5851e2dff0
Nils Kenneweg <nk@ovt.me>
2017-09-27 13:13:19 +0200

    switch to private is now a link

--- assets/js/sales/salesMain.ts
+++ assets/js/sales/salesMain.ts
@@ -1,10 +1,4 @@
-import { goToPrivate } from "../services/location.manager"
-
 require("../runners/serviceWorkerRunner")
-
-document.getElementById("switchToPrivate").addEventListener("click", function () {
-	goToPrivate()
-});
 
 document.getElementById("trial").addEventListener("click", function () {
 	alert("trial")
