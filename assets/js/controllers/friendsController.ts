71d70d75504723a0b1e646b437990ec97c7f5af2
Nils Kenneweg <nk@ovt.me>
2018-01-18 12:23:37 +0100

    code cleanup

--- assets/js/controllers/friendsController.ts
+++ assets/js/controllers/friendsController.ts
@@ -1,31 +1,31 @@
 import cssService from "../services/css.service"
-var friendsService = require("services/friendsService");
-var localize = require("i18n/localizationConfig");
+var friendsService = require("../services/friendsService")
+var localize = require("../i18n/localizationConfig")
 import userService from "../users/userService"
 
-const controllerModule = require("controllers/controllerModule");
+const controllerModule = require("../controllers/controllerModule")
 
 const friendsController: any = ($scope) => {
-	cssService.setClass("friendsView");
-	$scope.friends = [];
-	$scope.requests = [];
+	cssService.setClass("friendsView")
+	$scope.friends = []
+	$scope.requests = []
 	$scope.colleagues = []
-	$scope.friendsLoading = true;
+	$scope.friendsLoading = true
 	$scope.friendsFilter = {
 		name: ""
-	};
+	}
 
 	$scope.removeFriend = function (user) {
 		if (confirm(localize.getLocalizedString("magicbar.requests.confirmRemove", { user: user.name }))) {
-			user.user.removeAsFriend();
+			user.user.removeAsFriend()
 		}
-	};
+	}
 
 	function loadFriendsUsers() {
 		return userService.getMultipleFormatted(friendsService.getFriends())
 			.then((result) => {
-				$scope.friends = result;
-				$scope.friendsLoading = false;
+				$scope.friends = result
+				$scope.friendsLoading = false
 			})
 	}
 
@@ -35,19 +35,19 @@ const friendsController: any = ($scope) => {
 	}
 
 	friendsService.awaitLoading().then(function () {
-		friendsService.listen(loadFriendsUsers);
-		friendsService.listen(loadRequestsUsers);
-		loadFriendsUsers();
-		loadRequestsUsers();
-	});
+		friendsService.listen(loadFriendsUsers)
+		friendsService.listen(loadRequestsUsers)
+		loadFriendsUsers()
+		loadRequestsUsers()
+	})
 
 	$scope.acceptRequest = function (request) {
-		request.user.acceptFriendShip();
-	};
+		request.user.acceptFriendShip()
+	}
 
 	$scope.ignoreRequest = function (request) {
-		request.user.ignoreFriendShip();
-	};
+		request.user.ignoreFriendShip()
+	}
 }
 
-controllerModule.controller("ssn.friendsController", ["$scope", friendsController]);
+controllerModule.controller("ssn.friendsController", ["$scope", friendsController])
