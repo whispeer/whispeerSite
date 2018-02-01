18649a1027d04f81d6d36e199ccc8438073f05b2
Nils Kenneweg <nk@ovt.me>
2018-02-01 10:50:31 +0100

    do not show colleagues first

--- assets/js/controllers/friendsController.ts
+++ assets/js/controllers/friendsController.ts
@@ -24,7 +24,7 @@ const friendsController: any = ($scope) => {
 		name: ""
 	}
 
-	$scope.showColleagues = WHISPEER_BUSINESS
+	$scope.showColleagues = false
 
 	$scope.isBusiness = () => WHISPEER_BUSINESS
 
